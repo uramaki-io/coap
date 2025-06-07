bb6e39d24c4e7a5e7eac0794f63dbf3ea53855a5
Zbigniew Mandziejewicz <shaxbee@gmail.com>
2025-06-07 17:25:15 +0900

    fix: enable OptionDef.String() test

--- optiondef_test.go
+++ optiondef_test.go
@@ -17,20 +17,20 @@ func TestOptionDefMethods(t *testing.T) {
 			def:        IfMatch,
 			recognized: true,
 			critical:   true,
-			str:        "Option(Name=IfMatch, Code=1, ValueFormat=Opaque, Repeatable=false, MinLen=0, MaxLen=8)",
+			str:        "Option(Name=IfMatch, Code=1, ValueFormat=opaque, MinLen=0, MaxLen=8)",
 		},
 		{
 			name:       "no-cache-key",
 			def:        Size1,
 			recognized: true,
 			noCacheKey: true,
-			str:        "Option(Name=Size1, Code=1, ValueFormat=Uint, Repeatable=false, MinLen=0, MaxLen=0)",
+			str:        "Option(Name=Size1, Code=60, ValueFormat=uint, MinLen=0, MaxLen=4)",
 		},
 		{
 			name:   "unrecognized, unsafe",
 			def:    UnrecognizedOptionDef(MaxAge.Code, MaxOptionLength),
 			unsafe: true,
-			str:    "Option(Code=14, ValueFormat=Opaque, MaxLen=1024)",
+			str:    "Option(Code=14, ValueFormat=opaque, MaxLen=1024)",
 		},
 	}
 
@@ -39,14 +39,21 @@ func TestOptionDefMethods(t *testing.T) {
 			if got := test.def.Recognized(); got != test.recognized {
 				t.Errorf("Recognized() = %v, want %v", got, test.recognized)
 			}
+
 			if got := test.def.Critical(); got != test.critical {
 				t.Errorf("Critical() = %v, want %v", got, test.critical)
 			}
+
 			if got := test.def.Unsafe(); got != test.unsafe {
 				t.Errorf("Unsafe() = %v, want %v", got, test.unsafe)
 			}
+
 			if got := test.def.NoCacheKey(); got != test.noCacheKey {
 				t.Errorf("NoCacheKey() = %v, want %v", got, test.noCacheKey)
+			}
+
+			if got := test.def.String(); got != test.str {
+				t.Errorf("String() = %q, want %q", got, test.str)
 			}
 		})
 	}
